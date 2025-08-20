5409751b8f420ba36dcae5afdc3c4d23b4c5f536
abhi <aailabeu@gmail.com>
2025-08-20 21:01:14 +0530

    firstDay

---
+++ Day1/Hello.js
@@ -0,0 +1,25 @@
+// console.log("hello world!")
+
+// first repititive program to print something
+// for(let i=1;i<=10;i++){
+//     console.log("I am a billionaire.")
+// }
+
+let a=25
+console.log(a)
+const b="abhishek is  a billionaire"
+let m=10
+let n=35
+console.log(m+n)
+
+let x=5
+if(x>=18){
+    console.log("You are eligible to vote");
+}else{
+    console.log("You are not eligible to vote!")
+}
+
+// this is single line comment 
+/* this is multi line comment as
+we can comment multiple lines like this */
+
